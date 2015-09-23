ef2f2e2fc64b9b04a6c9b7e9c53148f9d140c552
Clément Gamé <clement.game@outscale.com>
2015-09-23 16:01:28 +0200

    v2.1 packaging.

---
+++ include/quotek/datasources/twitter.hpp
@@ -0,0 +1,72 @@
+/*
+Quotek Strategies SDK 2.0
+Copyright 2013-2015 Quotek SAS
+http://www.quotek.io
+*/
+
+#ifndef TWITTER_HPP
+#define TWITTER_HPP
+
+#include <string>
+#include "../datasource.hpp"
+#include "../news.hpp"
+#include "../timeutils.hpp"
+
+namespace quotek {
+
+  namespace datasource {
+   
+    /** Twitter is a class that allows to fetch twitter content from the regular API. */
+
+    class twitter: public datasource {
+
+      public:
+
+        /** twitter class constructor. To get all the constructor parameters, 
+         *  just go to https://apps.twitter.com/, create a new app and go to "Keys and Access Tokens".
+         *
+         *  @param consumer_key: Twitter app consumer key.
+         *  @param consumer_secret: Twitter app consumer secret.
+         *  @param token: Twitter app token.
+         *  @param token_secret: Twitter app token secret.
+         */
+      	twitter(std::string consumer_key, 
+                std::string consumer_secret,
+                std::string token,
+                std::string token_secret);
+
+      	/** twitter class destructor */
+      	~twitter();
+
+        /** fetches the last tweets of given user.
+         *
+         *  @param screen_name: The name of account to fetch tweets for.
+         *  @param nb_tweets: The number of tweets to fetch (sorted by descending date)
+      	 *  @return a vector of quotek news objects.
+         */     
+      	std::vector<quotek::data::news> fetch(std::string screen_name,
+                                              int nb_tweets);
+
+
+
+        /** searches the tweets matching the term provided as argument.
+         *
+         *  @param term: term to search in the global tweets feed?
+         *  @param nb_tweets: The number of found tweets to retrieve.
+         *  @return a vector of quotek news objects.
+         */
+
+        std::vector<quotek::data::news> search(std::string term,
+                                               int nb_tweets);
+
+
+      private:
+
+        void* tw_handler;
+
+    };
+
+  }
+}
+
+#endif
